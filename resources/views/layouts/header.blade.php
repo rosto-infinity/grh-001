2d3f29d111b32d7050475c4ded6254d512c6f1f3
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-03 22:37:43 +0100

    <!-- 03-Sidebar user panel (optional) -->

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -141,7 +141,7 @@
 
     <!-- 02Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
+      <!-- 03-Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
           <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
