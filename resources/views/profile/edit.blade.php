88224047deb38df39a03f8d774cca9e581474de2
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-03 20:40:24 +0100

    <!-- 02-Content Header (Page header) -->

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,7 +4,7 @@
 <!-- 01-Content Wrapper. Contains page content -->
 <div class="content-wrapper">
 
-    <!-- Content Header (Page header) -->
+    <!-- 02-Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
     <div class="py-12">
