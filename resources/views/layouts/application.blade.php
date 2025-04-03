c161f4a3f763b660d56ee365deee9c43c90ce939
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-03 22:04:54 +0100

    <!-- 05-Bootstrap 4 -->

--- resources/views/layouts/application.blade.php
+++ resources/views/layouts/application.blade.php
@@ -66,7 +66,7 @@
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
-<!-- Bootstrap 4 -->
+<!-- 05-Bootstrap 4 -->
 <script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- ChartJS -->
 <script src="{{asset('plugins/chart.js/Chart.min.js')}}"></script>
