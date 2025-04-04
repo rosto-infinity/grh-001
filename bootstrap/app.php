3ee6954733be1a6981b9104eff9efd5be8b4e30a
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-04 19:34:40 +0100

    withMiddleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
          
     })
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        //gg
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
