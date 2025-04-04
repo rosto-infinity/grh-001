05ef57587269cfce1225edda16172c92f606471a
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-04 19:29:29 +0100

    <!-- 28-Small boxes (Stat box) -->

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -49,7 +49,7 @@
      <!-- 27-Main content -->
      <section class="content">
        <div class="container-fluid">
-         <!-- Small boxes (Stat box) -->
+         <!-- 28-Small boxes (Stat box) -->
          <div class="row">
            <div class="col-lg-3 col-6">
              <!-- small box -->
