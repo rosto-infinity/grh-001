9e85642ae901474a093bd51f6b866eee02001a1b
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-04 19:29:08 +0100

    <!-- 26-Content Header (Page header) -->

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -28,7 +28,7 @@
  
    <!-- 25-Content Wrapper. Contains page content -->
    <div class="content-wrapper">
-     <!-- Content Header (Page header) -->
+     <!-- 26-Content Header (Page header) -->
      <div class="content-header">
        <div class="container-fluid">
          <div class="row mb-2">
