343d919742ee5ed3b30ca8677cc007debd862956
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-04 19:28:53 +0100

    <!-- 25-Content Wrapper. Contains page content -->

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -26,7 +26,7 @@
  @section('content')
  
  
-   <!-- Content Wrapper. Contains page content -->
+   <!-- 25-Content Wrapper. Contains page content -->
    <div class="content-wrapper">
      <!-- Content Header (Page header) -->
      <div class="content-header">
