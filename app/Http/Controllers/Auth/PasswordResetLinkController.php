8bf7323d129391ab0de24aaca9aab6ffd6e6f0d0
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-06 19:51:54 +0100

    // 20-to send the link, we will examine the response then see the message we

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -30,7 +30,7 @@ class PasswordResetLinkController extends Controller
         ]);
 
         // 19-We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
+        // 20-to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
         $status = Password::sendResetLink(
             $request->only('email')
