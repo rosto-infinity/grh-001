95319da3c5c14fb1d49e8d630550fdae2f670a8f
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-06 19:50:23 +0100

    // 13-database. Otherwise we will parse the error and return the response.

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -38,7 +38,7 @@ class NewPasswordController extends Controller
 
         // 11-Here we will attempt to reset the user's password. If it is successful we
         // 12-will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
+        // 13-database. Otherwise we will parse the error and return the response.
         $status = Password::reset(
             $request->only('email', 'password', 'password_confirmation', 'token'),
             function (User $user) use ($request) {
