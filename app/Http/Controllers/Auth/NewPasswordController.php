b6ec0c8bfe14eeb6492d64b16c0bc6bbb52b56ce
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-06 19:50:12 +0100

    // 12-will update the password on an actual user model and persist it to the

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -37,7 +37,7 @@ class NewPasswordController extends Controller
         ]);
 
         // 11-Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
+        // 12-will update the password on an actual user model and persist it to the
         // database. Otherwise we will parse the error and return the response.
         $status = Password::reset(
             $request->only('email', 'password', 'password_confirmation', 'token'),
