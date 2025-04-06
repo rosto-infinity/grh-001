daebb94a6021e930aaac308824a195a8721df112
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-06 19:48:51 +0100

    * 7-Confirm the user's password.

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -20,7 +20,7 @@ class ConfirmablePasswordController extends Controller
     }
 
     /**
-     * Confirm the user's password.
+     * 7-Confirm the user's password.
      */
     public function store(Request $request): RedirectResponse
     {
