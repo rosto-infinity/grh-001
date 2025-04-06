db2e56dad9bb649228c6f9fc209e09eea93db9a8
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-06 18:50:47 +0100

    * 02-The attributes that are mass assignable.

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * 02-The attributes that are mass assignable.
      *
      * @var list<string>
      */
