72466ce7d27dccce9e058f3778f4d0f13a86ab3d
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-06 19:49:09 +0100

    * 6-Send a new email verification notification.

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class EmailVerificationNotificationController extends Controller
 {
     /**
-     * Send a new email verification notification.
+     * 6-Send a new email verification notification.
      */
     public function store(Request $request): RedirectResponse
     {
