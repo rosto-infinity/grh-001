44dc4dc94dcb842b39b871a48c13e2cc4d1e49f2
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-06 19:51:34 +0100

    * 18-Handle an incoming password reset link request.

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -19,7 +19,7 @@ class PasswordResetLinkController extends Controller
     }
 
     /**
-     * Handle an incoming password reset link request.
+     * 18-Handle an incoming password reset link request.
      *
      * @throws \Illuminate\Validation\ValidationException
      */
