990d0daa01de23dd6e45d61f52602c2c29d8c935
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-07 21:43:22 +0100

    03-Colonne min_salary, par défaut null

--- database/migrations/2025_04_07_200337_create_jobs_table.php
+++ database/migrations/2025_04_07_200337_create_jobs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('jobs', function (Blueprint $table) {
             $table->id(); // 00-Colonne id, auto-incrémentée
             $table->string('job_title')->nullable(); // 001-Colonne job_title, par défaut null
-            $table->decimal('min_salary', 10, 2)->nullable(); // Colonne min_salary, par défaut null
+            $table->decimal('min_salary', 10, 2)->nullable(); // 03-Colonne min_salary, par défaut null
             $table->decimal('max_salary', 10, 2)->nullable(); // Colonne max_salary, par défaut null
             $table->timestamps(); // Colonne created_at et updated_at
         });
