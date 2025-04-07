3e132217d6f0d23e2258610deab95efad3770572
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-07 21:32:28 +0100

    upd

---
+++ public/errors/404.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page non trouvée - Erreur 404</title>
+</head>
+<body>
+    <h1>Page non trouvée - Erreur 404</h1>
+    <p>Désolé, la page que vous recherchez est introuvable.</p>
+</body>
+</html>
