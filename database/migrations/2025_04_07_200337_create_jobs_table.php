b6e6f25fdec8542d8d0483d00d044ce4b5c989db
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-07 21:49:15 +0100

    02-Reverse the migrations.

--- database/migrations/2025_04_07_200337_create_jobs_table.php
+++ database/migrations/2025_04_07_200337_create_jobs_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('jobs', function (Blueprint $table) {
             $table->id(); // 00-Colonne id, auto-incrémentée
             $table->string('job_title')->nullable(); // 001-Colonne job_title, par défaut null
-            $table->decimal('min_salary', 10, 2)->nullable(); // 03-Colonne min_salary, par défaut null
-            $table->decimal('max_salary', 10, 2)->nullable(); // 04-Colonne max_salary, par défaut null
+            $table->string('min_salary')->nullable(); // 03-Colonne min_salary, par défaut null
+            $table->string('max_salary')->nullable(); // 04-Colonne max_salary, par défaut null
             $table->timestamps(); // 05-Colonne created_at et updated_at
         });
     }
 
     /**
-     * Reverse the migrations.
+     * 02-Reverse the migrations.
      */
     public function down(): void
     {
