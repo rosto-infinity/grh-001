fa16feec2351636b908ec81552512ff8acf55fe5
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-18 21:51:00 +0100

    --Content Header (Page header)

--- resources/views/admin/emplois/list.blade.php
+++ resources/views/admin/emplois/list.blade.php
@@ -4,7 +4,7 @@
 
     <!-- --Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
+        <!-- ---Content Header (Page header) -->
         <section class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
