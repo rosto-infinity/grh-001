8d9cffd399c2061c2adde408d1ec92d17a9ea91b
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-18 01:52:38 +0100

    2- Run the migrations.

---
+++ database/migrations/2025_04_18_004028_create_emploi_histories_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     *2- Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('emploi_histories', function (Blueprint $table) {
+            $table->id();
+
+            // Relation avec les utilisateurs
+            $table->foreignId('user_id')
+                  ->nullable()
+                  ->constrained()
+                  ->onDelete('cascade');
+
+            // Relation avec un emploi
+            $table->foreignId('emploi_id')
+                  ->nullable()
+                  ->constrained()
+                  ->onDelete('cascade');
+
+            // Dates de début et de fin (de type date ou datetime, pas integer)
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('emploi_histories');
+    }
+};
