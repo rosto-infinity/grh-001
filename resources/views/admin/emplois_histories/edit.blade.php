27f5437f6dffd786aca41156fabf3bbd6f1f3f35
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-18 21:46:30 +0100

    -- Emploi

--- resources/views/admin/emplois_histories/edit.blade.php
+++ resources/views/admin/emplois_histories/edit.blade.php
@@ -65,7 +65,7 @@
                                         @enderror
                                     </div>
 
-                                    {{-- Emploi --}}
+                                    {{--- Emploi --}}
                                     <div class="form-group col-md-6">
                                         <label for="emploi_id">Emploi <span class="text-red-600">*</span></label>
                                         <select name="emploi_id" id="emploi_id"
@@ -83,7 +83,7 @@
                                         @enderror
                                     </div>
 
-                                    {{-- Date de début --}}
+                                    {{--- Date de début --}}
                                     <div class="form-group col-md-6">
                                         <label for="start_date">Date de début</label>
                                         <input type="date" name="start_date" id="start_date"
