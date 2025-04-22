9ffcb3861bd02c27cc7feeecf357ee99e454c02f
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-22 21:40:17 +0100

    --Routes protégées par authentification

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Routes protégées par authentification
+// --Routes protégées par authentification
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit'); // Éditer le profil
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update'); // Mettre à jour le profil
