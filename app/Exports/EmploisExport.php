31646fecd29a79f865239195a3efe06e0f3f33c0
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-22 19:03:19 +0100

    -Déclare la classe EmploisExport qui implémente l'interface FromView

--- app/Exports/EmploisExport.php
+++ app/Exports/EmploisExport.php
@@ -6,7 +6,7 @@ use App\Models\Emploi; // -n  Importe le modèle Emploi pour l'utiliser dans cet
 use Illuminate\Contracts\View\View; // --Importe l'interface View pour le type de retour
 use Maatwebsite\Excel\Concerns\FromView; // ---Importe l'interface FromView pour l'exportation Excel
 
-class EmploisExport implements FromView // Déclare la classe EmploisExport qui implémente l'interface FromView
+class EmploisExport implements FromView // -Déclare la classe EmploisExport qui implémente l'interface FromView
 {
     // Méthode qui retourne la vue à utiliser pour l'exportation
     public function view(): View
