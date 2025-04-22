401d87007a360ae172897ae3e1e03fb4234b3628
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-22 22:42:57 +0100

    Éditer le profil

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/dashboard', function () {
 
 // --Routes protégées par authentification
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit'); // Éditer le profil
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit'); //-- Éditer le profil
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update'); // Mettre à jour le profil
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy'); // Supprimer le profil
 });
