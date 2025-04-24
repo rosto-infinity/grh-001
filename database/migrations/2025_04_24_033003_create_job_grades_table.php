c4fd1d19daf4c2a6857660746f822632839ecca8
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-24 04:30:39 +0100

    -Colonne id auto-incrémentée

---
+++ database/migrations/2025_04_24_033003_create_job_grades_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_grades', function (Blueprint $table) {
+            $table->id(); // -Colonne id auto-incrémentée
+            $table->string('grade_level'); // Colonne pour le niveau de grade
+            $table->decimal('lowest_sal', 10, 2); // Colonne pour le salaire le plus bas
+            $table->decimal('highest_sal', 10, 2); // Colonne pour le salaire le plus élevé
+            $table->timestamps(); // Colonne pour les timestamps created_at et updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_grades');
+    }
+};
