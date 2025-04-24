d861dacec34b098f8484b6dd147e485f8ee62d73
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-24 04:56:28 +0100

    migration-mc

---
+++ app/Http/Controllers/EmploiGradeController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmploiGradeController extends Controller
+{
+    //
+}
--- database/migrations/2025_04_24_033556_create_emploi_grades_table.php
+++ database/migrations/2025_04_24_033556_create_emploi_grades_table.php
@@ -7,24 +7,24 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * -Run the migrations.
+     * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('job_grades', function (Blueprint $table) {
+        Schema::create('emploi_grades', function (Blueprint $table) {
             $table->id(); // -Colonne id auto-incrémentée
             $table->string('grade_level'); // -Colonne pour le niveau de grade
-            $table->decimal('lowest_sal', 10, 2); // -Colonne pour le salaire le plus bas
-            $table->decimal('highest_sal', 10, 2); // -Colonne pour le salaire le plus élevé
+            $table->decimal('lowest_salary', 10, 2); // -Colonne pour le salaire le plus bas
+            $table->decimal('highest_salary', 10, 2); // -Colonne pour le salaire le plus élevé
             $table->timestamps(); // -Colonne pour les timestamps created_at et updated_at
         });
     }
 
     /**
-     * -Reverse the migrations.
+     * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_grades');
+        Schema::dropIfExists('emploi_grades');
     }
 };
