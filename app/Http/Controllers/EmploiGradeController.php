bccf44b70f9c39f6ee6292655e9f6631b5e5a240
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-27 15:46:51 +0100

    -Supprimer le grade

--- app/Http/Controllers/EmploiGradeController.php
+++ app/Http/Controllers/EmploiGradeController.php
@@ -70,7 +70,7 @@ class EmploiGradeController extends Controller
     {
         // 1 - -Supprimer le grade par ID
         $grade = EmploiGrade::findOrFail($id); // -Trouver le grade ou échouer
-        $grade->delete(); // Supprimer le grade
+        $grade->delete(); // -Supprimer le grade
 
         // 2 - Rediriger ou retourner une réponse
         return redirect()->route('admin.emplois_grades')->with('success', 'Grade supprimé avec succès.');
