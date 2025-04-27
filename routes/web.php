cdd191531c4c35b89855f1d473294e33433f5380
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-27 13:40:34 +0100

    --Mettre à jour un grade d'emploi

--- routes/web.php
+++ routes/web.php
@@ -69,7 +69,7 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::post('admin/emplois_grades', [EmploiGradeController::class, 'store'])->name('admin.emplois_grades.store'); // 24 -- Enregistrer un nouveau grade d'emploi
     Route::get('admin/emplois_grades/{id}', [EmploiGradeController::class, 'show'])->name('admin.emplois_grades.show'); // 25 - -Voir les détails d'un grade d'emploi
     Route::get('admin/emplois_grades/{id}/edit', [EmploiGradeController::class, 'edit'])->name('admin.emplois_grades.edit'); // 26 - --Éditer un grade d'emploi
-    Route::patch('admin/emplois_grades/{id}', [EmploiGradeController::class, 'update'])->name('admin.emplois_grades.update'); // 27 - Mettre à jour un grade d'emploi
+    Route::patch('admin/emplois_grades/{id}', [EmploiGradeController::class, 'update'])->name('admin.emplois_grades.update'); // 27 - ---Mettre à jour un grade d'emploi
     Route::delete('admin/emplois_grades/{id}', [EmploiGradeController::class, 'destroy'])->name('admin.emplois_grades.destroy'); // 28 - Supprimer un grade d'emploi
 });
 
