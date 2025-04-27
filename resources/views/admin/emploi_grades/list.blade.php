18c94475ad8aeda6921484e765af5f954437e42a
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-27 15:37:53 +0100

    Filtrer par Date de création

--- resources/views/admin/emploi_grades/list.blade.php
+++ resources/views/admin/emploi_grades/list.blade.php
@@ -47,18 +47,20 @@
                                                 </label>
                                                 <input type="text" class="form-control" id="grade_level" name="grade_level" placeholder="Entrez votre grade_level" value="{{ Request::get('grade_level') }}">
                                             </div>
-                                 {{-- Filtrer par emploi --}}
-                         
+                                
+                                            {{-- Filtrer par lowest_salary --}}
                                             <div class="form-group col-md-3">
                                                 <label for="lowest_salary">lowest_salary</label>
                                                 <input type="number" class="form-control" id="lowest_salary" name="lowest_salary" placeholder="Entrez votre lowest_salary" value="{{ Request::get('lowest_salary') }}">
                                             </div>
                                 
+                                            {{-- Filtrer par Highest Salary --}}
                                             <div class="form-group col-md-3">
-                                                <label for="highest_salary">Max salary</label>
+                                                <label for="highest_salary">Highest Salary</label>
                                                 <input type="number" class="form-control" id="highest_salary" name="highest_salary" placeholder="highest_salary" value="{{ Request::get('highest_salary') }}">
                                             </div>
                                 
+                                            {{-- Filtrer par Date de création --}}
                                             <div class="form-group col-md-3">
                                                 <label for="date">Date de création</label>
                                                 <input type="date" class="form-control" id="date" name="date" value="{{ Request::get('date') }}">
@@ -91,9 +93,9 @@
                                 <table class="table table-striped">
                                     <thead>
                                         <tr>
-                                            <th class="bg-green-400 ">Job title</th>
-                                            <th>Min Salary</th>
-                                            <th>Max Salary</th>
+                                            <th class="bg-green-400 ">grade_level</th>
+                                            <th>lowest_salary</th>
+                                            <th>highest_salary</th>
                 
                                             <th>Date de création</th>
                                             <th>Action</th>
@@ -109,9 +111,9 @@
                                         @else
                                         @foreach ($emploi_grades as $emploi_grade)
                                             <tr> 
-                                                <td>{{ $emploi_grade->emploi_title }}</td>
+                                                <td>{{ $emploi_grade->grade_level}}</td>
                                                 <td>{{ $emploi_grade->lowest_salary }}</td>
-                                                <td>{{ $emploi_grade->max_salary }}</td>
+                                                <td>{{ $emploi_grade->highest_salary }}</td>
                                                
                                                 <td>{{ $emploi_grade->created_at->translatedFormat('l d/m/Y \à H\h:i') }}</td>
                                                 <td>
