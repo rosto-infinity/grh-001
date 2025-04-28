92865c99818b35d10d0a74307c12fd0107886052
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-28 20:18:14 +0100

    create controller et Model

--- routes/web.php
+++ routes/web.php
@@ -71,6 +71,15 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('admin/emplois_grades/{id}/edit', [EmploiGradeController::class, 'edit'])->name('admin.emplois_grades.edit'); // 26 - --Éditer un grade d'emploi
     Route::patch('admin/emplois_grades/{id}', [EmploiGradeController::class, 'update'])->name('admin.emplois_grades.update'); // 27 - ---Mettre à jour un grade d'emploi
     Route::delete('admin/emplois_grades/{id}', [EmploiGradeController::class, 'destroy'])->name('admin.emplois_grades.destroy'); // 28 - --Supprimer un grade d'emploi
+    //Regions
+    Route::get('admin/emplois_grades', [EmploiGradeController::class, 'index'])->name('admin.emplois_grades'); // 21 - Lister tous les grades d'emploi
+    Route::get('admin/emplois_grades/excel', [EmploiGradeController::class, 'excel'])->name('admin.emplois_grades.excel'); // 22 - --Exporter les grades d'emploi au format Excel
+    Route::get('admin/emplois_grades/add', [EmploiGradeController::class, 'add'])->name('admin.emplois_grades.add'); // 23 - -Formulaire de création d'un grade d'emploi
+    Route::post('admin/emplois_grades/add', [EmploiGradeController::class, 'store'])->name('admin.emplois_grades.store'); // 24 -- Enregistrer un nouveau grade d'emploi
+    Route::get('admin/emplois_grades/{id}', [EmploiGradeController::class, 'view'])->name('admin.emplois_grades.view'); // 25 - -Voir les détails d'un grade d'emploi
+    Route::get('admin/emplois_grades/{id}/edit', [EmploiGradeController::class, 'edit'])->name('admin.emplois_grades.edit'); // 26 - --Éditer un grade d'emploi
+    Route::patch('admin/emplois_grades/{id}', [EmploiGradeController::class, 'update'])->name('admin.emplois_grades.update'); // 27 - ---Mettre à jour un grade d'emploi
+    Route::delete('admin/emplois_grades/{id}', [EmploiGradeController::class, 'destroy'])->name('admin.emplois_grades.destroy'); // 28 - --Supprimer un grade d'emploi
 });
 
 
