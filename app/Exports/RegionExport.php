1eea166d1f42d67948be98a5a83fa9afcd5777ec
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-28 21:00:40 +0100

    -Méthode qui retourne la vue à utiliser pour l'exportation

--- app/Exports/RegionExport.php
+++ app/Exports/RegionExport.php
@@ -8,7 +8,7 @@ use Maatwebsite\Excel\Concerns\FromView; // -Importe l'interface FromView pour l
 
 class RegionExport implements FromView // D-éclare la classe RegionExport qui implémente l'interface FromView
 {
-    // Méthode qui retourne la vue à utiliser pour l'exportation
+    // -Méthode qui retourne la vue à utiliser pour l'exportation
     public function view(): View
     {
         // Récupération de toutes les régions depuis la base de données
