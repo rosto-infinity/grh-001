1758186b54b699b717307ce403fed068e36afd01
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-28 21:00:05 +0100

    -Importe le modèle Region pour l'utiliser dans cette classe

--- app/Exports/RegionExport.php
+++ app/Exports/RegionExport.php
@@ -2,7 +2,7 @@
 
 namespace App\Exports; // Déclare le namespace de la classe
 
-use App\Models\Region; // Importe le modèle Region pour l'utiliser dans cette classe
+use App\Models\Region; // -Importe le modèle Region pour l'utiliser dans cette classe
 use Illuminate\Contracts\View\View; // Importe l'interface View pour le type de retour
 use Maatwebsite\Excel\Concerns\FromView; // Importe l'interface FromView pour l'exportation Excel
 
