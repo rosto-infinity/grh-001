b16c985651c671f9004fda782bfab89e64dd141d
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-28 21:07:28 +0100

    -- Date de création

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -11,7 +11,7 @@ class Region extends Model
     // 1 --Les attributs qui peuvent être remplis en masse
     protected $fillable = [
         'region_name',  // -- Le nom de la région
-        'created_at',   // - Date de création
+        'created_at',   // -- Date de création
         'updated_at'    // - Date de mise à jour
     ];
 
