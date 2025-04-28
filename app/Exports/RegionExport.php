bdb8ea1b804b88c3d34f9e0459866c7e20216b96
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-28 21:00:13 +0100

    -Importe l'interface View pour le type de retour

--- app/Exports/RegionExport.php
+++ app/Exports/RegionExport.php
@@ -3,7 +3,7 @@
 namespace App\Exports; // Déclare le namespace de la classe
 
 use App\Models\Region; // -Importe le modèle Region pour l'utiliser dans cette classe
-use Illuminate\Contracts\View\View; // Importe l'interface View pour le type de retour
+use Illuminate\Contracts\View\View; // -Importe l'interface View pour le type de retour
 use Maatwebsite\Excel\Concerns\FromView; // Importe l'interface FromView pour l'exportation Excel
 
 class RegionExport implements FromView // Déclare la classe RegionExport qui implémente l'interface FromView
